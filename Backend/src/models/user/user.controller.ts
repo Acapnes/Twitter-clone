a6fbaa45777b33201e1fb28c6eaba63d9a734823
Alper <61701011+Acapnes@users.noreply.github.com>
2022-05-17 20:50:38 +0300

    Login Post requests

--- Backend/src/models/user/user.controller.ts
+++ Backend/src/models/user/user.controller.ts
@@ -1,17 +1,22 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
 import { User } from "src/schemas/user.schema";
 import { UsersService } from "./user.service";
-
+import { UserDto } from "src/dto/user.dto";
 
 
 @Controller('/users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) { }
 
   @Get()
   async getUsers(): Promise<User[]> {
     return this.usersService.getAll();
+  }
+
+  @Post('/login')
+  async userLogin(@Body() UserDto: UserDto) {
+    return this.usersService.Login(UserDto.email, UserDto.password);
   }
 
 }
--- Backend/src/models/user/user.service.ts
+++ Backend/src/models/user/user.service.ts
@@ -5,9 +5,13 @@ import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) { }
 
   async getAll(): Promise<User[]> {
     return this.userModel.find({});
+  }
+
+  async Login(email: string, password: string): Promise<User> {
+    return this.userModel.findOne({ email, password });
   }
 }
