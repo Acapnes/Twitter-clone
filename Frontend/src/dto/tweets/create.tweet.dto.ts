f2a0fdcddfd619077b2e820420b1fc425dd718a3
Alper <61701011+Acapnes@users.noreply.github.com>
2022-05-28 01:45:22 +0300

    Tweet DTOs

---
+++ Frontend/src/dto/tweets/create.tweet.dto.ts
@@ -0,0 +1,15 @@
+export interface CreateTweetDto {
+
+    author: string,
+
+    data: {
+        time: string
+        message:string
+        likeCount:number
+        retweetCount:number
+        commentCount:number
+        img:{
+            src:string
+        }
+    }
+}
---
+++ Frontend/src/dto/tweets/tweet.dto.ts
@@ -0,0 +1,24 @@
+export interface TweetDto {
+
+    author: {
+        _id:string,
+        name:string,
+        email:string,
+        username:string,
+        avatar:string,
+        confrimed:boolean,
+        bio:string,
+        password:string,
+    };
+
+    data: {
+        time: string;
+        message:string
+        likeCount:number
+        retweetCount:number
+        commentCount:number
+        img:{
+            src:string
+        }
+    }
+}
