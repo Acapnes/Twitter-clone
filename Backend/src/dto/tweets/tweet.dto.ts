8d5ecec732e44a7f80d8343b931ae930cec09b9a
Alper <61701011+Acapnes@users.noreply.github.com>
2022-06-03 16:38:40 +0300

    nullable

--- Backend/src/dto/tweets/tweet.dto.ts
+++ Backend/src/dto/tweets/tweet.dto.ts
@@ -3,18 +3,20 @@ import { UserDto } from "../users/user.dto";
 
 export class TweetDto {
     
+    _id?:string
+    
     @IsNotEmpty()
     author: UserDto
 
     @IsNotEmpty()
     data: {
-        time: string;
-        message:string
-        likeCount:number
-        retweetCount:number
-        commentCount:number
+        time?: string;
+        message?:string
+        likeCount?:number
+        retweetCount?:number
+        commentCount?:number
         img:{
-            src:string
+            src?:string
         }
     }
 
